49aacdcb9364614b5c87f0b8f648525f416cd332
Long Nguyen <nguyenv@brandeis.edu>
2026-03-17 01:23:06 -0700

    add shit

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/nodes', nodesRouter);
+app.get('/edges', (_req, res) => res.json([]));
 
 app.listen(3001, () => {
   console.log('Server on :3001');
